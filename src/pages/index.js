c6a94ffe0dbd6ba728f3f3bad06f7a40867ca80b
Vatsala Srivastava <vatsala2197@gmail.com>
2020-07-03 22:15:02 +0530

    name changes in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,12 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.dark.css';
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Hi people, I'm Vatsala Srivastava</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
