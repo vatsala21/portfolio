22ebdf65ad0debc7e962af4003ea5de7b93c4d96
Vatsala Srivastava <vatsala2197@gmail.com>
2020-07-03 22:18:38 +0530

    removed dark theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import 'antd/dist/antd.dark.css';
+import 'antd/dist/antd.css';
 
 const IndexPage = () => (
   <Layout>
